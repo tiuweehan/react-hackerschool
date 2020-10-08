024a7f392daf1f0956b4b0cc78529d7333504e4c
Tiu Wee Han <deantiu56@gmail.com>
2020-10-08 17:26:52 +0800

    2-interactive

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert(`clicked on square ${this.props.value}`)}>
         {this.props.value}
       </button>
     );
