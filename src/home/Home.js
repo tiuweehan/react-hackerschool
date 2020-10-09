7d3da6f666cb4a9f224bddc736863c23c6d1ad53
Tiu Wee Han <deantiu56@gmail.com>
2020-10-09 09:48:56 +0800

    9-homepage

---
+++ src/home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (<div>Welcome to Wee Han's homepage</div>)
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './home/Home';
 import Games from './games/Games';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Home />
         <Games />
       </div>
     )
