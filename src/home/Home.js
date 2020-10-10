e76a32ca366a97b3980886314804cfc667477543
Tiu Wee Han <deantiu56@gmail.com>
2020-10-10 10:37:10 +0800

    12-images

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,6 +4,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home">
+        <img src={"/images/background.jpg"} alt={"background"} height={'500px'}/>
         <h1>Welcome to Wee Han's homepage</h1>
       </div>
     )
